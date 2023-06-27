4c4b9653e3676580b3a7d339fe65d2a82b7287ab
Clarice Domingo <125780532+ClariceDomingo@users.noreply.github.com>
2023-06-28 00:44:43 +0800

    Add files via upload

---
+++ Kitty Korner Website/js/6order.js
@@ -0,0 +1,13 @@
+function showPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+
+function hidePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
+
+function stopPropagation(event) {
+    event.stopPropagation();
+}
